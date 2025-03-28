814f119493c8e13b52eebb4d822b9926fb3a3760
PrajaktaWakhare2116 <prajaktawakhare21@gmail.com>
2025-03-28 20:01:24 +0530

    reCaptcha validation

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate.value" ,inputdate.value);
+let capchachecked = false;
 
-    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); // this gives the date in indian  format
-    outputdate.value = formatedDate;
+
+function beforesubmit(event){
+    if(capchachecked){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("inputdate.value" ,inputdate.value);
+
+        let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); // this gives the date in indian  format
+        outputdate.value = formatedDate;
+    }else{
+        alert("Please checked the reCAPTCHA box to submit the lead");
+        event.preventDefault();
+    }
+    
 }  
 
-function timestamp() 
-{ var response = document.getElementById("g-recaptcha-response"); 
+function timestamp() {
+     var response = document.getElementById("g-recaptcha-response"); 
     if (response == null || response.value.trim() == "") {
         var elems = JSON.parse(
             document.getElementsByName("captcha_settings")[0].value
@@ -17,3 +26,7 @@ function timestamp()
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } }
      setInterval(timestamp, 500); 
+
+function captchasuccess()  {
+    capchachecked = true;
+}   
